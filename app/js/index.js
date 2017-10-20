298a92614398b6f66d862125966f598a8032d626
Abhishek Jha <abhjha@deloitte.com>
2017-10-21 04:21:04 +0530

    cookies removed
    removed cookie implementation

--- app/js/index.js
+++ app/js/index.js
@@ -9,21 +9,13 @@ userRole = {
   initEvents: function() {
     $(".admin-control").on("click", () => {
       window.location.href = "html/dashboard.html";
-      this.setCookie("userRole", "admin");
       sessionStorage.setItem('userRole', 'admin');
     });
 
     $(".user-control").on("click", () => {
       window.location.href = "html/dashboard.html";
-      this.setCookie("userRole", "user");
       sessionStorage.setItem('userRole', 'user');
     });
-  },
-
-  setCookie: function(key, value) {
-    let expires = new Date();
-    expires.setTime(expires.getTime() + 1 * 24 * 60 * 60 * 1000);
-    document.cookie = key + "=" + value + "; expires=" + expires.toGMTString() + "; path=/";
   }
 };
 
