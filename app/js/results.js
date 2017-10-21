9acfb1367cca7fc1bf6f542e18b8cd26c7251138
praveendivekar <praveendivekar@gmail.com>
2017-10-21 12:36:06 +0530

    Results page including graph

---
+++ app/js/results.js
@@ -0,0 +1,37 @@
+var ctx = document.getElementById("chartContainer");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+        datasets: [{
+            label: 'Results of the Survey',
+            data: [10, 5, 3, 1, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
